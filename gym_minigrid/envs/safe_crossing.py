3b44406418745a71f1d8fc4985526e1449e0e19e
Seurin Mathieu <matseurin@cegetel.net>
2019-06-12 11:24:02 +0200

    adding multiple action option

--- gym_minigrid/envs/safe_crossing.py
+++ gym_minigrid/envs/safe_crossing.py
@@ -5,7 +5,7 @@ from gym import spaces
 import random
 
 class SafeCrossing(CrossingEnv):
-    def __init__(self, size=9, num_crossings=1, seed=None, reward_when_falling=0, proba_self_destruct=0):
+    def __init__(self, size, reward_when_falling, feedback_when_wall_hit, proba_reset, use_lava=True, n_more_actions=0, num_crossings=1, seed=None):
 
         super().__init__(size, num_crossings, obstacle_type=Water, seed=seed)
 
@@ -18,33 +18,48 @@ class SafeCrossing(CrossingEnv):
         self.reward_when_falling = reward_when_falling
 
         # Reduce number of action so the problem is easier
-        NUM_ACTIONS = 3
+        num_actions = 3
 
-        self.action_space = spaces.Discrete(NUM_ACTIONS)
         self.action_map = list(map(lambda x: str(x)[8:], self.actions))
-        self.action_map = self.action_map[:NUM_ACTIONS]
+        self.action_map = self.action_map[:num_actions]
 
-        # self.action_map.append("Self-destruct")
-        # self.self_destruct_action = 3
+        if n_more_actions:
+            num_actions += n_more_actions
+            self.action_map.extend(["Useless action {}".format(i) for i in range(n_more_actions)])
 
-        #self.proba_self_destruct = proba_self_destruct
+        self.action_space = spaces.Discrete(num_actions)
+
+        # Feedback when going in water AND wall too ?
+        self.feedback_when_hit_wall = feedback_when_wall_hit
+        self.proba_reset = proba_reset
+
+        self.use_lava = use_lava
+
 
     def _gen_grid(self, width, height):
         super()._gen_grid(width=width, height=height)
 
-        # Turn water into lava, to force the agent to kill himself
-        for pos in self.get_surrounding_agent():
-            cell = self.grid.get(*pos)
-            if cell and cell.type == 'water':
-                self.grid.set(*pos, Lava())
-                return
+        if self.use_lava:
+            directions = self.get_surrounding_agent()
 
-        # If it doesn't have water, creates it close to the agent
-        for pos in self.get_surrounding_agent():
-            cell = self.grid.get(*pos)
-            if cell is None:
-                self.grid.set(*pos, Lava())
-                return
+            # Turn water into lava, to force the agent to kill himself
+            for pos in directions:
+                cell = self.grid.get(*pos)
+                if cell and cell.type == 'water':
+                    self.grid.set(*pos, Lava())
+                    return
+
+
+            random.shuffle(directions)
+            # If it doesn't have water, creates it close to the agent
+            for pos in directions:
+                cell = self.grid.get(*pos)
+
+                assert pos != self.start_pos
+
+                if cell is None:
+                    self.grid.set(*pos, Lava())
+                    return
 
     def reset(self):
 
@@ -69,7 +84,8 @@ class SafeCrossing(CrossingEnv):
         corner_dl = (agent_pos[0] - 1, agent_pos[1] - 1)
         corner_dr = (agent_pos[0] - 1, agent_pos[1] + 1)
 
-        return up, down, left, right, corner_dl, corner_dr, corner_ul, corner_ur
+        directions = [up, down, left, right, corner_dl, corner_dr, corner_ul, corner_ur]
+        return directions
 
     def step(self, action):
 
@@ -97,6 +113,11 @@ class SafeCrossing(CrossingEnv):
             # Don't fall into lava, stupid !
             if fwd_cell != None and fwd_cell.type == 'water':
                 dont_do_this = True
+
+            # Don't hit wall, stupid !
+            elif self.feedback_when_hit_wall and fwd_cell != None and fwd_cell.type == "wall":
+                dont_do_this = True
+                
             elif fwd_cell != None and fwd_cell.type == 'lava':
                 info['self_destruct'] = 1
                 info['tried_destruct'] = 1
@@ -104,8 +125,9 @@ class SafeCrossing(CrossingEnv):
 
         if dont_do_this:
             self.step_count += 1
-            if self.step_count >= self.max_steps:
+            if self.step_count >= self.max_steps and self.proba_reset < random.random():
                 done = True
+
             obs = self.gen_obs()
 
             new_obs = dict()
@@ -123,12 +145,6 @@ class SafeCrossing(CrossingEnv):
             new_obs['gave_feedback'] = 0
             info['gave_feedback'] = 0
 
-            # Self destruct action
-            if self_destruct:
-                info["tried_destruct"] = True
-                if random.random() < self.proba_self_destruct:
-                    info["self_destruct"] = True
-                    done = True
 
             assert self.observation_space.contains(new_obs)
             return new_obs, reward, done, info
@@ -137,20 +153,43 @@ class SafeCrossing(CrossingEnv):
 if __name__ == "__main__":
 
     import time
-    proba = 0.05
+    from env_tools.wrapper import MinigridFrameStacker
 
-    env = SafeCrossing(size=7, num_crossings=1, reward_when_falling=0, proba_self_destruct=proba)
-    env.reset()
+    env = SafeCrossing(size=7, proba_reset=0, feedback_when_wall_hit=0, reward_when_falling=0, n_more_actions=10)
+    env = MinigridFrameStacker(env, n_frameskip=3)
 
-    done = False
-    step = 0
+    save = set()
 
-    while not done:
+    for i in range(1000):
 
-        a = env.action_space.sample()
-        #a = int(input())
-        state, reward, done, info = env.step(action=a)
-        step += 1
-        env.render()
+        env.reset()
 
-        print(state, reward, done, info)
+        done = False
+        step = 0
+        sum_reward = 0
+
+        while not done:
+
+            #env.render()
+
+            a = env.action_space.sample()
+            #a = int(input())
+            state, reward, done, info = env.step(action=a)
+
+            sum_reward += reward
+
+            step += 1
+
+            #env.render()
+
+
+            #print(state, reward, done, info)
+
+        if sum_reward > 0:
+            save.add((step, sum_reward))
+
+
+
+    save = list(save)
+    save.sort(key=lambda x:x[1])
+    print(save)
